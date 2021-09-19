58f4a0cea9bfbceba2f2e8b86118ac45b582afc1
Abid <49099853+Abidarian@users.noreply.github.com>
2021-09-20 04:14:41 +0530

    Add files via upload

---
+++ printNumbers.ino
@@ -0,0 +1,32 @@
+/*
+ 
+
+  The printNumber function uses two overloaded functions that keep your column-numbers
+  steady, regardless of size or sign, by converting each number to a string then
+  pre-padding each column-width with spaces. The column-widths, which are currently
+  set to 6 may be altered by changing each "6" to "whatever-the-number-will-fit-into".
+
+  The overloaded functions accept "float" and "short" numbers.
+
+
+*/
+
+// ----- Routine to stop integer numbers jumping around
+long printNumber(short number) {
+  String myString = String(number);
+  short numberChars = myString.length();
+  for (short i = 0; i < 6 - numberChars; i++) {
+    Serial.print(" ");
+  }
+  Serial.print(myString);
+}
+
+// ----- Routine to stop floats jumping around
+float printNumber(float number) {
+  String myString = String(number);
+  short numberChars = myString.length();
+  for (short i = 0; i < 6 - numberChars; i++) {
+    Serial.print(" ");
+  }
+  Serial.print(myString);
+}
